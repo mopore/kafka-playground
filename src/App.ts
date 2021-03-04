78343ef9ce1588935e84f9a2c7e067ed4d57140a
mopore <jni@mopore.org>
2021-03-04 16:58:59 +0100

    Setup to run kafka in docker locally.

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,28 @@
-console.log('Ready...');
+import { HighLevelProducer, KafkaClient } from "kafka-node";
+
+console.log('Connecting to Kafka server...');
+
+
+const client = new KafkaClient(
+    {
+        kafkaHost: 'localhost:9092',
+        connectTimeout: 1000
+    }
+);
+const producer = new HighLevelProducer(client);
+const payloads = [
+    { topic: 'Topic1', messages: 'hi' },
+    { topic: 'Topic2', messages: ['hello', 'world']}
+];
+producer.on('ready', () => {
+    producer.send(payloads, (error, data) => {
+        if (error){
+            const errorMessage = `Error sending payload to kafka server: ${error}`;
+            console.error(errorMessage);
+            console.trace();
+        }
+        else {
+            console.log(`Successfully send data to Kafka: ${data}`);
+        }
+    });
+});
